fdb51e7b9589b9ea3562481c8e38310e1ce452ab
Colin <colint.cron@gmail.com>
2025-07-24 18:49:54 +0000

    Added PyQt6 and updated requirements.txt

--- FFT_in_Python/pythonfft.py
+++ FFT_in_Python/pythonfft.py
@@ -1,16 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 
-t = np.arange(100)
-s = np.sin(0.15*2*np.pi*t)
+Fs = 1 #Hz
+N = 100 # number of points to simulate, and our FFT size
 
-S = np.fft.fft(s)
-# print(S) 
+t = np.arange(N)
+s = np.sin(0.15*2*np.pi*t)
+S = np.fft.fftshift(np.fft.fft(s))
 
 S_mag = np.abs(S)
 S_phase = np.angle(S)
-plt.plot(t,S_mag,'.-')
-plt.savefig("../images/fftmagnitude.svg", bbox_inches='tight')
-plt.clf()
-plt.plot(t,S_phase,'.-')
-plt.savefig("../images/fftphase.svg", bbox_inches='tight')
+f = np.arange(Fs/-2, Fs/2, Fs/N)
+
+plt.figure(0)
+plt.plot(f,S_mag,'.-')
+plt.figure(1)
+plt.plot(f,S_phase,'.-')
+plt.show()
